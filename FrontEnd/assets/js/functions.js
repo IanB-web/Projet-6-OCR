e587eb4c476c93fae7db3cc567350b4146ff721e
Ian <deus.nightshade@gmail.com>
2023-11-16 20:26:48 +0100

    BUILD: ajout boutons de filtres ( html + css + JS)

--- FrontEnd/assets/js/functions.js
+++ FrontEnd/assets/js/functions.js
@@ -1,16 +1,29 @@
 const galleryNode = document.querySelector(".gallery");
-
+const filtersNode = document.querySelector(".filters");
 /**
  * récupérer les données de la route /api/works
  */
 async function getWorks() {
-  const results = await fetch("http://localhost:5678/api/works").then(
+  const worksResults = await fetch("http://localhost:5678/api/works").then(
     (reponse) => {
       return reponse.json();
     }
   );
-  console.log(results);
-  return results;
+  console.log(worksResults);
+  return worksResults;
+}
+
+/**
+ * récupérer les données de la route /api/categories
+ */
+async function getFilters() {
+  const filtersResults = await fetch("http://localhost:5678/api/categories").then(
+    (reponse) => {
+      return reponse.json();
+    }
+  );
+  console.log(filtersResults);
+  return filtersResults;
 }
 
 /**
@@ -32,18 +45,37 @@ function displayWorks(dataWorks) {
 }
 
 /**
+ * Parcours les filtres et les affiches dans le DOM
+ */
+function displayFilters(dataFilters) {
+  for (let i = 0; i < dataFilters.length; i++) {
+    // Création des balises dédiées à chaque projet
+    const filter = document.createElement("button");
+    filter.setAttribute ("type","button")
+    filter.classList.add("btn");
+    filter.classList.add(`filter${dataFilters[i].id}`)
+
+    filter.innerHTML = `
+    ${dataFilters[i].name}
+    `;
+
+    filtersNode.appendChild(filter);
+  }
+}
+
+/**
  *
  */
 async function init() {
+  // Affichage des projets
   const works = await getWorks();
   displayWorks(works);
 
-  // Affichage des catégories à rajouter
+  // Affichage des filtres
+  const filters = await getFilters();
+  displayFilters(filters);
 }
 
 init();
 
-/**
- * TODO :
- * - Faire pareil pour afficher les boutons de filtres catégories
- */
+
