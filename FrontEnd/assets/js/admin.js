edc53d94f294340238eab42c823f5fe887ed3529
IanB-web <deus.nightshade@gmail.com>
2023-12-07 11:23:41 +0100

    BUILD: ajout éléments modal

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -4,12 +4,11 @@ const token = localStorage.getItem("token");
 const editionBand = document.querySelector(".edition");
 const logLink = document.querySelector(".log");
 const openModalBtn = document.querySelector(".change__btn");
-
+const modal = document.querySelector(".modal");
 
 
 // conditionnement token pour affichage de l'index "créateur"
 if (token) {
-
   // on affiche les élements de base montrant que l'on peut éditer du contenu
   openModalBtn.style.display = "flex";
   editionBand.classList.remove("hidden");
@@ -28,7 +27,57 @@ if (token) {
   });
 
   // création de la fonction click pour l'apparition de la modale
-  openModalBtn.addEventListener("click", function() {
-    
-  })
+  openModalBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  modal.style.display = "block"
+  });
+
+  /**
+   *  remplissage de la modale avec les tous projets existants 
+   **/
+const modalGallery = document.querySelector(".modal__gallery")
+
+  // Récupération des travaux(projets) présents dans le backend
+async function getWorks() {
+  const worksResults = await fetch("http://localhost:5678/api/works").then(
+    (reponse) => {
+      return reponse.json();
+    }
+  );
+  return worksResults;
+}
+
+// Parcours des travaux et les affiches dans la modale
+function resetDisplayModalGallery() {
+  modalGallery.innerHTML = "";
+}
+function displayWorks(works) {
+  // on réinitialise la galerie des projets
+  resetDisplayModalGallery();
+
+  works.forEach(work => displayWork(work))
+}
+
+function displayWork(work) {
+  // Création des balises dédiées à chaque projet
+  const projet = document.createElement("article");
+  projet.classList.add("modal__box");
+
+  projet.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}" />
+      <p><i class="fa-solid fa-trash-can"></i></p>
+    `;
+
+  modalGallery.appendChild(projet);
+}
+
+async function modalInit() {
+  // Affichage des projets
+  const works = await getWorks();
+  displayWorks(works);}
+
+
+modalInit();
+
+
 }
